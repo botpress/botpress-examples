3c716cfc138941a86caa762ad580e98c8d7c81a8
Sylvain Perron <slvn.perron+github@gmail.com>
2016-12-03 14:11:26 -0500

    First iteration working

--- motivation-bot/index.js
+++ motivation-bot/index.js
@@ -1,3 +1,99 @@
+const Promise = require('bluebird')
+const _ = require('lodash')
+const videos = require('./videos')
+
+const TEXT_CATEGORIES = {
+  WORK: [
+    "Oh, yeah, work isn't always easy man. Let's fix that right away.",
+    "Listen to that, I bet you'll get a raise next week 💪"
+  ],
+  LIFE: [
+    "Take charge of your life NOW"
+  ],
+  GYM: [
+    "Want muscles?"
+  ]
+}
+
+const pickCategory = {
+  quick_replies: [
+    {
+      content_type: 'text',
+      title: '🔥 Work 🔥',
+      payload: 'GET_VIDEO_WORK'
+    },
+    {
+      content_type: 'text',
+      title: '😌 Life Goals 🔥',
+      payload: 'GET_VIDEO_LIFE'
+    },
+    {
+      content_type: 'text',
+      title: '💪 Gym 🔥',
+      payload: 'GET_VIDEO_GYM'
+    }
+  ],
+  typing: true
+}
+
 module.exports = function(bp) {
   bp.middlewares.load()
+
+  bp.hear({
+    type: 'postback',
+    text: 'GET_STARTED' 
+  }, (event, next) => {
+    const { first_name, last_name } = event.user
+    bp.logger.info('New user:', first_name, last_name)
+
+    const WELCOME_SENTENCES = [
+      "Hey there buddy pal, so I've heard that you need a little kick in the butt from time to time?",
+      "Don't worry mate, that's my job and I'll do that for you.",
+      "But man, I don't talk much.",
+      "I'm a bit dumb, to be honest. Let's just stick to using buttons, that's going to be easier for the both of us."
+    ]
+
+    const WELCOME_TEXT_QUICK_REPLY = "THAT BEING SAID, choose a category right away and I'll make sure you get pumped up!"
+
+    Promise.mapSeries(WELCOME_SENTENCES, txt => {
+      bp.messenger.sendText(event.user.id, txt, { typing: true })
+      return Promise.delay(4000)
+    })
+    .then(() => {
+      bp.messenger.sendText(event.user.id, WELCOME_TEXT_QUICK_REPLY, pickCategory)
+    })
+  })
+
+  const hearGetVideo = category => {
+    bp.hear({ text: 'GET_VIDEO_' + category }, (event, next) => {
+      const text = _.sample(TEXT_CATEGORIES[category])
+      bp.messenger.sendText(event.user.id, text, { typing: true })
+
+      Promise.delay(1500)
+      .then(() => videos.getRandomVideo(category))
+      .then(meta => {
+        bp.messenger.sendTemplate(event.user.id, {
+          template_type: 'generic',
+          elements: [{
+            title: meta.title,
+            item_url: meta.url,
+            image_url: meta.thumbnail,
+            subtitle: meta.description,
+            buttons: [
+              { type: 'web_url', title: '🔥 Watch 🔥', url: meta.url },
+              { type: 'element_share' }
+            ]
+          }]
+        })
+      })
+    })
+  }
+
+  // Create a listener for each categories
+  _.keys(TEXT_CATEGORIES).forEach(hearGetVideo)
+
+  bp.botDefaultResponse = event => {
+    const text = event.user.first_name + ", I told you, I'm a bit dumb. I assume you want motivation, cause that's all I'm able to do :)"
+    bp.messenger.sendText(event.user.id, text, pickCategory)
+  }
 }
---
+++ motivation-bot/videos.js
@@ -0,0 +1,71 @@
+const axios = require('axios')
+const _ = require('lodash')
+
+const videos = {
+  WORK: [
+    "DNFtCIzJQ7A",
+    "g-fGYtagSDY",
+    "00rPgc0tISM",
+    "qa9G5EdKiRw",
+    "3AyH1fBN7ac",
+    "HQtZ4kud2qA",
+    "puDQoBPpWyQ",
+    "A0Scr2TW2ZA",
+    "oonaeUiKV8Y",
+    "2zzj4CO9xSw",
+    "twZgNP8iZBQ",
+    "8QlvQC4MXxs"
+  ],
+  LIFE: [
+    "jE-Ajfd51aM",
+    "6vuetQSwFW8",
+    "7_R4AsV2fPI",
+    "njQcOKOpFwk",
+    "5g0QHTcwP8k",
+    "t-H7_aAuiC8",
+    "5e338_RFOr8",
+    "U3V701IUZ2E",
+    "ZkabeHig_r4",
+    "FK16iXPRAjI",
+    "4pxiU89O1wE",
+    "2otRxX6y7mQ"
+  ],
+  GYM: [
+    "Fh-rCrREEgA",
+    "xoXYe9e01_Y",
+    "hV63DbQ_qSc",
+    "YxzQ6umhH4Q",
+    "lsSC2vx7zFQ",
+    "63Cv6Jiiink",
+    "aMGoxlXmA0o",
+    "at7QvbFy9fM",
+    "qapsrR8zIJM",
+    "vnMtpNhcDOE",
+    "OV6-n5wtCWA",
+    "lpVRxa9jsrE",
+    "WDJaJbc9O-4"
+  ]
+}
+
+const getYoutubeVideoMetadata = (videoId) => {
+  const YOUTUBE_API_KEY = 'AIzaSyAp2kmHzUFdlD1b4N4XR0OhKUWnC_IVaAA'
+  const apiUrl = `https://content.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet&key=${YOUTUBE_API_KEY}`
+  
+  return axios.get(apiUrl)
+  .then(res => {
+    const video = res.data.items[0].snippet
+    return {
+      description: video.description,
+      thumbnail: (video.thumbnails.high || video.thumbnails.standard).url,
+      title: video.title,
+      url: 'https://www.youtube.com/watch?v=' + videoId
+    }
+  })
+}
+
+module.exports = {
+  getRandomVideo: (category) => {
+    const videoId = _.sample(videos[category])
+    return getYoutubeVideoMetadata(videoId)
+  }
+}
